017872cff542e288713456acfd499eed0e3e1c87
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-07-13 20:15:14 -0500

    Finished tutorial

---
+++ SpriteKitSimpleGame/SpriteKitSimpleGame/GameOverScene.h
@@ -0,0 +1,15 @@
+//
+//  GameOverScene.h
+//  SpriteKitSimpleGame
+//
+//  Created by Hector Enrique Gomez Morales on 7/13/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+-(id)initWithSize:(CGSize)size won:(BOOL)won;
+
+@end
