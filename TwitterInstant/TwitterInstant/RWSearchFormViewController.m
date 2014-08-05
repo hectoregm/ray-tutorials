d346f7cd2547cca0cdbdeb6cabbfc9c96c6409ea
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-08-04 23:13:22 -0500

    Create signal to wrap the result of user granting twitter access

--- TwitterInstant/TwitterInstant/RWSearchFormViewController.m
+++ TwitterInstant/TwitterInstant/RWSearchFormViewController.m
@@ -9,12 +9,24 @@
 #import "RWSearchFormViewController.h"
 #import "RWSearchResultsViewController.h"
 #import <ReactiveCocoa.h>
+#import "RACEXTScope.h"
+#import <Accounts/Accounts.h>
+#import <Social/Social.h>
+
+typedef NS_ENUM(NSInteger, RWTwitterInstantError) {
+  RWTwitterInstantErrorAccessDenied,
+  RWTwitterInstantErrorNoTwitterAccounts,
+  RWTwitterInstantErrorInvalidResponse
+};
+
+static NSString * const RWTwitterInstantDomain = @"TwitterInstant";
 
 @interface RWSearchFormViewController ()
 
 @property (weak, nonatomic) IBOutlet UITextField *searchText;
-
 @property (strong, nonatomic) RWSearchResultsViewController *resultsViewController;
+@property (strong, nonatomic) ACAccountStore *accountStore;
+@property (strong, nonatomic) ACAccountType *twitterAccountType;
 
 @end
 
@@ -30,12 +42,24 @@
   
   self.resultsViewController = self.splitViewController.viewControllers[1];
   
+  @weakify(self)
   [[self.searchText.rac_textSignal
    map:^id(NSString *text) {
       return [self isValidSearchText:text] ? [UIColor whiteColor] : [UIColor yellowColor];
    }]
    subscribeNext:^(UIColor *color) {
+     @strongify(self)
      self.searchText.backgroundColor = color;
+   }];
+  
+  self.accountStore = [[ACAccountStore alloc] init];
+  self.twitterAccountType = [self.accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+  
+  [[self requestAccessToTwitterSignal]
+   subscribeNext:^(id x) {
+     NSLog(@"Access granted");
+   } error:^(NSError *error) {
+     NSLog(@"An error ocurred %@", error);
    }];
 }
 
@@ -48,6 +72,28 @@
   textFieldLayer.borderColor = [UIColor grayColor].CGColor;
   textFieldLayer.borderWidth = 2.0f;
   textFieldLayer.cornerRadius = 0.0f;
+}
+
+- (RACSignal *)requestAccessToTwitterSignal {
+  NSError *accessError = [NSError errorWithDomain:RWTwitterInstantDomain
+                                             code:RWTwitterInstantErrorAccessDenied
+                                         userInfo:nil];
+  
+  @weakify(self)
+  return [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
+    @strongify(self)
+    [self.accountStore requestAccessToAccountsWithType:self.twitterAccountType
+                                               options:nil
+                                            completion:^(BOOL granted, NSError *error) {
+                                              if (!granted) {
+                                                [subscriber sendError:accessError];
+                                              } else {
+                                                [subscriber sendNext:nil];
+                                                [subscriber sendCompleted];
+                                              }
+                                            }];
+    return nil;
+  }];
 }
 
 @end
