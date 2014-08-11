7d2006cda14124ecbfb729d1b524030af422b494
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-08-10 20:18:18 -0500

    Add desaceleration to pan gesture

--- MonkeyPinch/MonkeyPinch/ViewController.swift
+++ MonkeyPinch/MonkeyPinch/ViewController.swift
@@ -20,6 +20,26 @@ class ViewController: UIViewController {
     recognizer.view.center = CGPoint(x: recognizer.view.center.x + translation.x,
       y: recognizer.view.center.y + translation.y)
     recognizer.setTranslation(CGPointZero, inView: self.view)
+    
+    if recognizer.state == .Ended {
+      let velocity = recognizer.velocityInView(self.view)
+      let magnitude = sqrt((velocity.x * velocity.x) + (velocity.y * velocity.y))
+      let slideMultiplier = magnitude / 200
+      println("magnitude: \(magnitude), slideMultiplier: \(slideMultiplier)")
+      
+      let slideFactor = 0.1 * slideMultiplier
+      var finalPoint = CGPoint(x: recognizer.view.center.x + (velocity.x * slideFactor),
+        y: recognizer.view.center.y + (velocity.y * slideFactor))
+      
+      finalPoint.x = min(max(finalPoint.x, 0), self.view.bounds.size.width)
+      finalPoint.y = min(max(finalPoint.y, 0), self.view.bounds.size.height)
+      
+      UIView.animateWithDuration(Double(slideFactor * 2),
+        delay: 0,
+        options: .CurveEaseOut,
+        animations: {recognizer.view.center = finalPoint},
+        completion: nil)
+    }
   }
 }
 
