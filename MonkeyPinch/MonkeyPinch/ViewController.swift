8a3bd90a60936df230ee38ef07445a8198d2b6a7
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-08-14 22:58:45 -0500

    Add simultaneous gesture recognizers

--- MonkeyPinch/MonkeyPinch/ViewController.swift
+++ MonkeyPinch/MonkeyPinch/ViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
                             
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -50,6 +50,10 @@ class ViewController: UIViewController {
   @IBAction func handleRotate(recognizer: UIRotationGestureRecognizer) {
     recognizer.view.transform = CGAffineTransformRotate(recognizer.view.transform, recognizer.rotation)
     recognizer.rotation = 0
+  }
+  
+  func gestureRecognizer(gestureRecognizer: UIGestureRecognizer!, shouldRecognizeSimultaneouslyWithGestureRecognizer otherGestureRecognizer: UIGestureRecognizer!) -> Bool {
+    return true
   }
 }
 
