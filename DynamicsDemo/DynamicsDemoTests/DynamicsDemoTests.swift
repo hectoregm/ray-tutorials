a738bffd26c77907150d807ab05aefcdc147f2ae
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-08-20 23:15:07 -0500

    Initial commit DyamicsDemo tutorial

---
+++ DynamicsDemo/DynamicsDemoTests/DynamicsDemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  DynamicsDemoTests.swift
+//  DynamicsDemoTests
+//
+//  Created by Hector Enrique Gomez Morales on 8/20/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class DynamicsDemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
