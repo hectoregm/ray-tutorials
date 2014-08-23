dbec407eb61f599af7b4205d08b298957cf0a0ae
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-08-22 22:22:56 -0500

    Add gravity behavior

--- DynamicsDemo/DynamicsDemo/ViewController.swift
+++ DynamicsDemo/DynamicsDemo/ViewController.swift
@@ -9,12 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
+  var animator: UIDynamicAnimator!
+  var gravity: UIGravityBehavior!
                             
   override func viewDidLoad() {
     super.viewDidLoad()
     let square = UIView(frame: CGRect(x: 100, y: 100, width: 100, height: 100))
     square.backgroundColor = UIColor.grayColor()
     view.addSubview(square)
+    
+    animator = UIDynamicAnimator(referenceView: view)
+    gravity = UIGravityBehavior(items: [square])
+    animator.addBehavior(gravity)
   }
 
   override func didReceiveMemoryWarning() {
