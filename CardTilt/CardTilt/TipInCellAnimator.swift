763c63fe42e08799c9569b6cfd9e92504114107e
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-09-10 23:29:46 -0500

    Add rotation animation to CardTilt

--- CardTilt/CardTilt/TipInCellAnimator.swift
+++ CardTilt/CardTilt/TipInCellAnimator.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import QuartzCore
 
 class TipInCellAnimator {
   class func animate(cell:UITableViewCell) {
     let view = cell.contentView
-    view.layer.opacity = 0.1
-    UIView.animateWithDuration(1.4) {
-      view.layer.opacity = 1
+    let rotationDegrees: CGFloat = -15.0
+    let rotationRadians: CGFloat = rotationDegrees * (CGFloat(M_PI)/180.0)
+    let offset = CGPointMake(-20, -20)
+    var startTransform = CATransform3DIdentity
+    startTransform = CATransform3DRotate(CATransform3DIdentity, rotationRadians, 0.0, 0.0, 1.0)
+    startTransform = CATransform3DTranslate(startTransform, offset.x, offset.y, 0.0)
+    
+    view.layer.transform = startTransform
+    view.layer.opacity = 0.8
+    
+    UIView.animateWithDuration(0.4) {
+      view.layer.transform = CATransform3DIdentity
+      view.layer.opacity = 1.0
     }
   }
 }
