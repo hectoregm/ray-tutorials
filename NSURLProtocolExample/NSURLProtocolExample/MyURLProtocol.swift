605a587d619a0a9244106329fe3291a0acbae5db
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-09-18 23:19:15 -0500

    Add URL loading

--- NSURLProtocolExample/NSURLProtocolExample/MyURLProtocol.swift
+++ NSURLProtocolExample/NSURLProtocolExample/MyURLProtocol.swift
@@ -11,8 +11,30 @@ import UIKit
 var requestCount = 0
 
 class MyURLProtocol: NSURLProtocol {
+  var connection: NSURLConnection!
+  
   override class func canInitWithRequest(request: NSURLRequest) -> Bool {
     println("Request #\(requestCount++): URL = \(request.URL.absoluteString)")
-    return false
+    return true
+  }
+  
+  override class func canonicalRequestForRequest(request: NSURLRequest) -> NSURLRequest {
+    return request
+  }
+  
+  override class func requestIsCacheEquivalent(aRequest: NSURLRequest, toRequest bRequest: NSURLRequest) -> Bool {
+    return super.requestIsCacheEquivalent(aRequest, toRequest: bRequest)
+  }
+  
+  override func startLoading() {
+    self.connection = NSURLConnection(request: self.request, delegate: self)
+  }
+  
+  override func stopLoading() {
+    if self.connection != nil {
+      self.connection.cancel()
+    }
+    
+    self.connection = nil
   }
 }
