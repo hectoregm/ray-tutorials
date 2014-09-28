e60d05846e091d131387947b47bea254f353a01e
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-09-28 14:44:29 -0500

    Add more functionality to OrderedDictionary

--- FlickrSearch/FlickrSearch/OrderedDictionary.swift
+++ FlickrSearch/FlickrSearch/OrderedDictionary.swift
@@ -14,4 +14,70 @@ struct OrderedDictionary<KeyType: Hashable, ValueType> {
   
   var array = ArrayType()
   var dictionary = DictionaryType()
+  
+  var count: Int {
+    return self.array.count
+  }
+  
+  mutating func insert(value: ValueType, forKey key: KeyType, atIndex index: Int) -> ValueType? {
+    var adjustedIndex = index
+    
+    let existingValue = self.dictionary[key]
+    if existingValue != nil {
+      let existingIndex = find(self.array, key)!
+      
+      if existingIndex < index {
+        adjustedIndex--
+      }
+      
+      self.array.removeAtIndex(existingIndex)
+    }
+    
+    self.array.insert(key, atIndex: adjustedIndex)
+    self.dictionary[key] = value
+    
+    return existingValue
+  }
+  
+  mutating func removeAtIndex(index: Int) -> (KeyType, ValueType) {
+    precondition(index < self.array.count, "Index out-of-bounds")
+    
+    let key = self.array.removeAtIndex(index)
+    let value = self.dictionary.removeValueForKey(key)!
+    
+    return (key, value)
+  }
+  
+  subscript(key: KeyType) -> ValueType? {
+    get {
+      return self.dictionary[key]
+    }
+    set {
+      if let index = find(self.array, key) {
+      } else {
+        self.array.append(key)
+      }
+      
+      self.dictionary[key] = newValue
+    }
+  }
+  
+  subscript(index: Int) -> (KeyType, ValueType) {
+    get {
+      precondition(index < self.array.count, "index out-of-bounds")
+      
+      let key = self.array[index]
+      let value = self.dictionary[key]!
+      
+      return (key, value)
+    }
+    set {
+      precondition(index <= self.array.count, "index out-of-bounds")
+      
+      let (key, value) = newValue
+
+      self.array[index] = key
+      self.dictionary[newValue.0] = value
+    }
+  }
 }
