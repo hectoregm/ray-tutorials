b8a47e46832ff886ce5dcb24c0dbff532ca7edb9
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-10-01 22:57:29 -0500

    Add new tutorial

---
+++ AdaptiveWeather/AdaptiveWeather/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AdaptiveWeather
+//
+//  Created by Hector Enrique Gomez Morales on 10/1/14.
+//  Copyright (c) 2014 Hector Enrique Gomez Morales. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
