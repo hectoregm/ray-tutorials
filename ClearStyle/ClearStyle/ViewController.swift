ff0e3c3799b9e816d828264a2d7f319f74530a3f
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-04 16:49:31 -0600

    Add style to cells for clear app

--- ClearStyle/ClearStyle/ViewController.swift
+++ ClearStyle/ClearStyle/ViewController.swift
@@ -20,10 +20,14 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         tableView.dataSource = self
         tableView.delegate = self
         tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
+      
+        tableView.separatorStyle = .None
+        tableView.rowHeight = 50.0
+      
         if toDoItems.count > 0 {
             return
         }
+      
         toDoItems.append(ToDoItem(text: "feed the cat"))
         toDoItems.append(ToDoItem(text: "buy eggs"))
         toDoItems.append(ToDoItem(text: "watch WWDC videos"))
@@ -55,6 +59,18 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
             let item = toDoItems[indexPath.row]
             cell.textLabel!.text = item.text
             return cell
+    }
+  
+    // MARK: - Table view delegate
+  
+    func colorForIndex(index: Int) -> UIColor {
+      let itemCount = toDoItems.count - 1;
+      let val = (CGFloat(index) / CGFloat(itemCount)) * 0.6
+      return UIColor(red: 1.0, green: val, blue: 0.0, alpha: 1.0)
+    }
+  
+    func tableView(tableView: UITableView, willDisplayCell cell: UITableViewCell, forRowAtIndexPath indexPath: NSIndexPath) {
+      cell.backgroundColor = colorForIndex(indexPath.row)
     }
 
 }
