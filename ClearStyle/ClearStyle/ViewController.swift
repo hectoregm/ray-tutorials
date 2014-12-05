9419d41a7f959bb5d2e5675f2cca96d75eb8b68a
Hector E. Gomez Morales <hectoregm@gmail.com>
2014-12-05 17:39:07 -0600

    More styling of the cells.

--- ClearStyle/ClearStyle/ViewController.swift
+++ ClearStyle/ClearStyle/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         tableView.dataSource = self
         tableView.delegate = self
-        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.registerClass(TableViewCell.self, forCellReuseIdentifier: "cell")
       
         tableView.separatorStyle = .None
         tableView.rowHeight = 50.0
@@ -55,9 +55,10 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     func tableView(tableView: UITableView,
         cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
             let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
-                as UITableViewCell
+                as TableViewCell
             let item = toDoItems[indexPath.row]
             cell.textLabel!.text = item.text
+            cell.textLabel!.backgroundColor = UIColor.clearColor()
             return cell
     }
   
